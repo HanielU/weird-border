b0775522b88b3339ef5321b6f7cc1a81ebb3ad87
Haniel Ubogu <hanieltobi@gmail.com>
2022-11-22 10:30:26 +0100

    initial commit

---
+++ uno.config.ts
@@ -0,0 +1,46 @@
+import {
+  defineConfig,
+  extractorSvelte,
+  presetIcons,
+  presetUno,
+  presetAttributify,
+  transformerDirectives,
+  transformerVariantGroup,
+  transformerCompileClass,
+} from "unocss";
+
+// https://github.com/unocss/unocss/tree/main/packages/vite
+// https://github.com/unocss/unocss/tree/main/packages/vite#svelte
+// https://github.com/unocss/unocss/tree/main/packages/preset-uno
+// https://github.com/unocss/unocss/tree/main/packages/preset-attributify
+// https://github.com/unocss/unocss/tree/main/packages/preset-icons
+// https://github.com/unocss/unocss/tree/main/packages/transformer-directives
+// https://github.com/unocss/unocss/tree/main/packages/transformer-variant-group
+
+// https://github.com/unocss/unocss#configurations
+export default defineConfig({
+  extractors: [extractorSvelte],
+
+  // https://github.com/unocss/unocss#extend-theme
+  theme: {},
+
+  // https://github.com/unocss/unocss#custom-rules
+  rules: [],
+
+  // https://github.com/unocss/unocss#shortcuts
+  shortcuts: {},
+
+  // https://github.com/unocss/unocss#using-presets
+  presets: [
+    presetUno(),
+    presetIcons({
+      scale: 1.2,
+    }),
+    presetAttributify(),
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+    transformerCompileClass(),
+  ],
+});
